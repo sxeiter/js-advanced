3654fd7ffea5a27f53b26b21d3a9c97b5fd5b03d
sxeiter <sxeiter@mail.ru>
2024-12-09 14:31:48 +0500

    дз по ооп

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 'use strict';
 
-const dateString1 = '2002-11-20';
-
-function checkAge(dateString) {
-    const parts = dateString.split('-');
-    const birthDate = new Date(parts[0], parts[1] - 1, parts[2]);
-    const currentDate = new Date();
-
-    let age = currentDate.getFullYear() - birthDate.getFullYear();
-
-    if (currentDate.getMonth() < birthDate.getMonth() ||
-        (currentDate.getMonth() === birthDate.getMonth() && 
-        currentDate.getDate() < birthDate.getDate())) {
-        age--; 
-    }
-
-    if (age >= 14) {
-        console.log('Вам есть 14 лет!');
-    } else {
-        console.log('Вам нет 14 лет!');
-    }
+function Character(race, name, language) {
+    this.race = race;
+    this.name = name;
+    this.language = language;
 }
 
-checkAge(dateString1);
+Character.prototype.speak = function() {
+    console.log(`${this.language}: ${this.name}`);
+};
+
+function Orc(name, language, weapon) {
+    Character.call(this, 'Орк', name, language);
+    this.weapon = weapon;
+}
+
+Orc.prototype = Object.create(Character.prototype);
+Orc.prototype.constructor = Orc;
+
+Orc.prototype.hit = function() {
+    console.log(`${this.name} наносит удар с оружием ${this.weapon}!`);
+};
+
+Orc.prototype.castSpell = function() {
+    console.log(`${this.name} использует заклинание!`);
+};
+
+Orc.prototype.createSpell = function(spellName) {
+    console.log(`${this.name} создает заклинание: ${spellName}`);
+};
+
+const orc1 = new Orc('Грог', 'Оркский', 'Топор');
+orc1.speak();
+orc1.hit();
+orc1.castSpell();
+orc1.createSpell('Огненный шар');
