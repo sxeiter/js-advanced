954a9c3cbfc685efa814c9094344bc74ef5fd63f
sxeiter <sxeiter@mail.ru>
2024-12-11 21:41:47 +0500

    добавил проверку окончания даты)

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 'use strict';
 
-const newYearDate = new Date('January 1, 2025 00:00:00').getTime();
+const newYearDate = new Date('January 1, 2026 00:00:00').getTime();
+
+function correctEnd(number, title) {
+    number = Math.abs(number);
+    let cases= [2, 0, 1, 1, 1, 2];
+    return title[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]]
+}
 
 function updateCountdown() {
     const now = new Date().getTime();
@@ -12,7 +18,13 @@ function updateCountdown() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    const countdownString = `${months} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`;
+    const monthTitle = correctEnd(months, ['месяц', 'месяца', 'месяцев']);
+    const dayTitle = correctEnd(days, ['день', 'дня', 'дней']);
+    const hourTitle = correctEnd(hours, ['час', 'часа', 'часов']);
+    const minuteTitle = correctEnd(minutes, ['минута', 'минуты', 'минут']);
+    const secondTitle = correctEnd(seconds, ['секунда', 'секунды', 'секунд']);
+
+    const countdownString = `${months} ${monthTitle}, ${days} ${dayTitle}, ${hours} ${hourTitle}, ${minutes} ${minuteTitle}, ${seconds} ${secondTitle}`;
 
     document.getElementById("countdown").innerText = countdownString;
 
