872a7811931fcd44e15a5ab14e20abeba0d906d0
Pavel Baksheev <sxeiter@mail.ru>
2025-02-04 18:45:50 +0500

    добавил старый код для сравнения

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
 'use strict';
 
+// function getPokemonData(pokemonName, callback) { 
+//     const request = new XMLHttpRequest(); 
+//     request.open('GET', `https://pokeapi.co/api/v2/pokemon/${pokemonName}`, true); 
+//     request.onload = function () { 
+//         if (request.status >= 200 && request.status < 300) { 
+//             const data = JSON.parse(request.responseText); 
+//             callback(null, data); 
+//         } else { 
+//             callback(`Ошибка: ${request.status}`); 
+//         } 
+//     }; 
+//     request.onerror = function () { 
+//         callback('Ошибка сети'); 
+//     }; 
+//     request.send(); 
+// } 
+// function getAbilityData(abilityUrl) { 
+//     const request = new XMLHttpRequest(); 
+//     request.open('GET', abilityUrl, true); 
+//     request.onload = function () { 
+//         if (request.status >= 200 && request.status < 300) { 
+//             const abilityData = JSON.parse(request.responseText); 
+//             console.log('Информация о способности:', abilityData); 
+//         } else { 
+//             console.error(`Ошибка: ${request.status}`); 
+//         } 
+//     }; 
+//     request.onerror = function () { 
+//         console.error('Ошибка сети'); 
+//     }; 
+// } 
+// getPokemonData('ditto', function (error, data) { 
+//     if (error) { 
+//         console.error(error); 
+//     } else { 
+//         console.log(data); 
+//         const abilityUrl = data.abilities[0].ability.url; 
+//         console.log('URL способности:', abilityUrl); 
+//         getAbilityData(abilityUrl); 
+//     } 
+// });
+
 function getPokemonData(pokemonName) {
     return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
         .then(response => {
