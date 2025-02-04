a94382e08c0c7eda98ce900f79e833c686a4334d
Pavel Baksheev <sxeiter@mail.ru>
2025-02-04 13:14:47 +0500

    дз по solid

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
 'use strict';
 
-class Book {
-    
+class Billing {
+    constructor(amount) {
+        this.amount = amount;
+    }
 
+    calculateTotal() {
+        throw new Error("Метод calculateTotal должен быть переопределен в подклассе");
+    }
+}
+
+class FixedBilling extends Billing {
+    constructor(amount) {
+        super(amount);
+    }
+
+    calculateTotal() {
+        return this.amount;
+    }
+}
+
+class HourBilling extends Billing {
+    constructor(amount, hours) {
+        super(amount);
+        this.hours = hours;
+    }
+
+    calculateTotal() {
+        return this.amount * this.hours;
+    }
+}
+
+class ItemBilling extends Billing {
+    constructor(amount, items) {
+        super(amount);
+        this.items = items;
+    }
+
+    calculateTotal() {
+        return this.amount * this.items;
+    }
 }
