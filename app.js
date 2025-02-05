036816b8f9ee17b50ab098d552f5c2e4123cb27a
Pavel Baksheev <sxeiter@mail.ru>
2025-02-05 13:46:12 +0500

    дз по геолокации

--- app.js
+++ app.js
@@ -1,90 +1,43 @@
-'use strict';
+'use strict'
 
-// function getPokemonData(pokemonName, callback) { 
-//     const request = new XMLHttpRequest(); 
-//     request.open('GET', `https://pokeapi.co/api/v2/pokemon/${pokemonName}`, true); 
-//     request.onload = function () { 
-//         if (request.status >= 200 && request.status < 300) { 
-//             const data = JSON.parse(request.responseText); 
-//             callback(null, data); 
-//         } else { 
-//             callback(`Ошибка: ${request.status}`); 
-//         } 
-//     }; 
-//     request.onerror = function () { 
-//         callback('Ошибка сети'); 
-//     }; 
-//     request.send(); 
-// } 
-// function getAbilityData(abilityUrl) { 
-//     const request = new XMLHttpRequest(); 
-//     request.open('GET', abilityUrl, true); 
-//     request.onload = function () { 
-//         if (request.status >= 200 && request.status < 300) { 
-//             const abilityData = JSON.parse(request.responseText); 
-//             console.log('Информация о способности:', abilityData); 
-//         } else { 
-//             console.error(`Ошибка: ${request.status}`); 
-//         } 
-//     }; 
-//     request.onerror = function () { 
-//         console.error('Ошибка сети'); 
-//     }; 
-// } 
-// getPokemonData('ditto', function (error, data) { 
-//     if (error) { 
-//         console.error(error); 
-//     } else { 
-//         console.log(data); 
-//         const abilityUrl = data.abilities[0].ability.url; 
-//         console.log('URL способности:', abilityUrl); 
-//         getAbilityData(abilityUrl); 
-//     } 
-// });
-
-function getPokemonData(pokemonName) {
-    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Ошибка: ${response.status}`);
+function getCurrentLocation(callback) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                callback(null, position);
+            },
+            (error) => {
+                callback(error);
             }
-            return response.json();
-        })
-        .catch(error => {
-            console.error('Произошла ошибка:', error);
-        });
+        );
+    } else {
+        callback(new Error("Геолокация не поддерживается этим браузером."));
+    }
 }
 
-function getAbilityData(abilityUrl) {
-    return fetch(abilityUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Ошибка: ${response.status}`);
+function getCurrentLocationPromise() {
+    return new Promise((resolve, reject) => {
+        getCurrentLocation((error, position) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(position);
             }
-            return response.json();
-        })
-        .catch(error => {
-            console.error('Произошла ошибка:', error);
         });
-}
-
-function getAbilityEffectDescription(abilityData) {
-    const englishEffect = abilityData.effect_entries.find(entry => entry.language.name === 'en');
-    return englishEffect ? englishEffect.effect : 'Описание отсутствует';
-}
-
-getPokemonData('ditto')
-    .then(data => {
-        console.log('Информация о покемоне:', data);
-        const abilityUrl = data.abilities[0].ability.url;
-        console.log('URL способности:', abilityUrl);
-        return getAbilityData(abilityUrl);
-    })
-    .then(abilityData => {
-        console.log('Информация о способности:', abilityData);
-        const description = getAbilityEffectDescription(abilityData);
-        console.log('Описание эффекта способности:', description);
-    })
-    .catch(error => {
-        console.error('Произошла ошибка:', error);
     });
+}
+
+document.getElementById("getLocationButton").addEventListener("click", () => {
+    getCurrentLocationPromise()
+        .then((position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            document.getElementById("locationOutput").textContent = `Широта: ${latitude}, Долгота: ${longitude}`;
+        })
+        .catch((error) => {
+            document.getElementById("locationOutput").textContent = `Ошибка: ${error.message}`;
+        })
+        .finally(() => {
+            console.log("Запрос геолокации завершен.");
+        });
+});
