3d6938eaac62dd1cc2cfa369b4ff222399c7fb41
Pavel Baksheev <sxeiter@mail.ru>
2025-02-11 15:00:43 +0500

    дз по race

--- app.js
+++ app.js
@@ -1,43 +1,77 @@
 'use strict'
+// получение геолокации
+// function getMyCoordinates() {
+//     return new Promise((resolve, reject) => {
+//         navigator.geolocation.getCurrentPosition(
+//             ({ coords }) => {
+//                 resolve({ 
+//                     latitude: coords.latitude,
+//                     longitude: coords.longitude,
+//                 });
+//             },
+//             (err) => {
+//                 reject(err);
+//             }
+//         );
+//     });
+// }
 
-function getCurrentLocation(callback) {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                callback(null, position);
-            },
-            (error) => {
-                callback(error);
-            }
-        );
-    } else {
-        callback(new Error("Геолокация не поддерживается этим браузером."));
+// async function getMyCity() {
+//     try {
+//         const { latitude, longitude } = await getMyCoordinates();
+//         const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`);
+//         if (!response.ok) {
+//             throw new Error(response.status)
+//         }
+//         const data = await response.json();
+//         console.log(data);
+//     } catch(e) {
+//         console.log(e);
+//     }
+// }
+
+// getMyCity();
+
+// const wrapper = document.querySelector('.wrapper');
+
+// async function getActivity() {
+//     const res = await fetch('https://bored.api.lewagon.com/api/activity');
+//     return res.json();
+// }
+
+// async function generate() {
+//     try {
+//         wrapper.innerHTML = '';
+//         const data = await Promise.all([
+//             getActivity(),
+//             getActivity(),
+//             getActivity(),
+//         ]);
+//         console.log(data);
+//         for (const el of data) {
+//             const element = document.createElement('div');
+//             element.innerHTML = `${el.activity}`;
+//             wrapper.appendChild(element);
+//         }
+//     } catch(e) {
+//         console.error(e)
+//     }
+// }
+
+
+async function race(promises) {
+    if (!Array.isArray(promises)) {
+        throw new Error('Должен быть массив промисов');
     }
-}
 
-function getCurrentLocationPromise() {
     return new Promise((resolve, reject) => {
-        getCurrentLocation((error, position) => {
-            if (error) {
+        promises.forEach(async (promise) => {
+            try {
+                const result = await Promise.resolve(promise);
+                resolve(result);
+            } catch (error) {
                 reject(error);
-            } else {
-                resolve(position);
             }
         });
     });
 }
-
-document.getElementById("getLocationButton").addEventListener("click", () => {
-    getCurrentLocationPromise()
-        .then((position) => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            document.getElementById("locationOutput").textContent = `Широта: ${latitude}, Долгота: ${longitude}`;
-        })
-        .catch((error) => {
-            document.getElementById("locationOutput").textContent = `Ошибка: ${error.message}`;
-        })
-        .finally(() => {
-            console.log("Запрос геолокации завершен.");
-        });
-});
