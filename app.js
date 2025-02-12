6d877751bec4c63fbca0aec526324da49907fd43
Pavel Baksheev <sxeiter@mail.ru>
2025-02-12 19:57:58 +0500

    дз по продвинутому DOM

--- app.js
+++ app.js
@@ -1,77 +1,20 @@
 'use strict'
-// получение геолокации
-// function getMyCoordinates() {
-//     return new Promise((resolve, reject) => {
-//         navigator.geolocation.getCurrentPosition(
-//             ({ coords }) => {
-//                 resolve({ 
-//                     latitude: coords.latitude,
-//                     longitude: coords.longitude,
-//                 });
-//             },
-//             (err) => {
-//                 reject(err);
-//             }
-//         );
-//     });
-// }
 
-// async function getMyCity() {
-//     try {
-//         const { latitude, longitude } = await getMyCoordinates();
-//         const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`);
-//         if (!response.ok) {
-//             throw new Error(response.status)
-//         }
-//         const data = await response.json();
-//         console.log(data);
-//     } catch(e) {
-//         console.log(e);
-//     }
-// }
+const buttonContainer = document.getElementById('buttonContainer');
+const counterDisplay = document.getElementById('counter');
+let count = 0;
+let lastButton = null;
 
-// getMyCity();
+buttonContainer.addEventListener('click', function(event) {
+    if (event.target.classList.contains('button')) {
+        count++;
 
-// const wrapper = document.querySelector('.wrapper');
+        if (lastButton) {
+            lastButton.textContent = 'Нажми меня';
+        }
+        lastButton = event.target;
+        lastButton.textContent = 'Нажата';
 
-// async function getActivity() {
-//     const res = await fetch('https://bored.api.lewagon.com/api/activity');
-//     return res.json();
-// }
-
-// async function generate() {
-//     try {
-//         wrapper.innerHTML = '';
-//         const data = await Promise.all([
-//             getActivity(),
-//             getActivity(),
-//             getActivity(),
-//         ]);
-//         console.log(data);
-//         for (const el of data) {
-//             const element = document.createElement('div');
-//             element.innerHTML = `${el.activity}`;
-//             wrapper.appendChild(element);
-//         }
-//     } catch(e) {
-//         console.error(e)
-//     }
-// }
-
-
-async function race(promises) {
-    if (!Array.isArray(promises)) {
-        throw new Error('Должен быть массив промисов');
+        counterDisplay.textContent = `Количество нажатий: ${count}`;
     }
-
-    return new Promise((resolve, reject) => {
-        promises.forEach(async (promise) => {
-            try {
-                const result = await Promise.resolve(promise);
-                resolve(result);
-            } catch (error) {
-                reject(error);
-            }
-        });
-    });
-}
+});
